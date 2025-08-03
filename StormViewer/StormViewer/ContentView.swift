fe3c37080cf6360722471abd52eed8cd032edf66
ER-Walker <erwalker816@icloud.com>
2025-08-03 12:13:31 -0400

    StormViewer
    Initial commit for the Storm Viewer project. Commit includes all initial files provided by Xcode.

---
+++ StormViewer/StormViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StormViewer
+//
+//  Created by Everett Walker on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StormViewer/StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Everett Walker on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
